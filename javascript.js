86ca75abb9aa0943021cfdbc42ddb225ffcda6c9
Janizs Kristóf <janizs.kristof11@gmail.com>
2024-09-22 20:30:17 +0200

    game logic reworked and workig kida

--- javascript.js
+++ javascript.js
@@ -1,47 +1,10 @@
-getComputerChoice = () => ["rock", "paper", "scissors"][Math.floor(Math.random()*3)];
-
-getPlayerChoice = () => {
-    let choice = prompt("Choose one: rock, paper, scissors").toLowerCase();
-    return choice === "rock" || choice === "paper" || choice === "scissors" ? choice : getPlayerChoice()
-}
-
-playRound = (h,c) => {
-    if ((h === "paper" && c === "rock") || (h === "rock" && c === "scissors") || (h === "scissors" && c === "paper")) return "win"
-    else if (h === c) return "draw"
-    else return "loss"
-}
-
-playGame = (rounds) => {
-    let humanScore = 0;
-    let computerScore = 0;
-    let h,c
-
-    for (let i=0; i<rounds; i++){
-        h = getPlayerChoice()
-        c = getComputerChoice()
-        let outcome = playRound(h,c)
-
-        if (outcome === "win") {
-            humanScore++
-            alert(`You won ${h} beats ${c}`)
-        } else if (outcome === "loss") {
-            computerScore++
-            alert(`You won ${h} beats ${c}`)
-        } else alert("It's a draw")
-    }
-    
-}
-
-
-//playGame(5);
-
+//-----------------MENU-----------------
 
 //mode dropdown menu
-
 const modeButton = document.querySelector('.mode');
 const dropdownContent = document.querySelector('.dropdown-content')
 const mode = document.querySelector('.mode')
-const rounds = document.querySelectorAll('.rounds')
+const roundsSelection = document.querySelectorAll('.rounds')
 
 
 modeButton.addEventListener('click', ()=>{
@@ -51,9 +14,63 @@ modeButton.addEventListener('click', ()=>{
     else dropdownContent.setAttribute("style","display:block;")
 });
 
-rounds.forEach((item) =>{
+//selecting the mode
+roundsSelection.forEach((item) =>{
     item.addEventListener('click', () =>{
         mode.textContent = 'Mode: ' + item.textContent
         dropdownContent.setAttribute("style","display:none;")
     })
 });
+
+//play button
+//switchig overlays
+
+const playButton = document.querySelector('.play-button')
+const menuContainer = document.querySelector('.menu-container')
+const gameContainer = document.querySelector('.game-container')
+
+const rounds = parseInt(mode.textContent.at(-1))
+playButton.addEventListener('click', ()=>{
+    menuContainer.setAttribute('style', 'display:none;');
+    gameContainer.setAttribute('style', 'display:flex;')
+    
+})
+
+//selecting computer weapon
+getComputerChoice = () => ["sword", "bow", "shield"][Math.floor(Math.random()*3)];
+
+
+//simulating round logic
+playRound = (h,c) => {
+    if ((h === "bow" && c === "sword") || (h === "shield" && c === "bow") || (h === "sword" && c === "shield")) return "win"
+    else if (h === c) return "draw"
+    else return "loss"
+}
+
+
+//playig the game
+const hands = document.querySelectorAll('.hands .player_');
+let playedRounds = 0;
+let humanScore = 0;
+let computerScore = 0;
+
+const scoreDisplay = document.querySelector('.score-display');
+
+
+hands.forEach((hand)=>{
+    hand.addEventListener('click', ()=>{
+        playerChoice = hand.firstChild.alt
+        //hand.firstChild.setAttribute('style', 'border:1px; border-style:solid; border-color: green')
+
+        let outcome = playRound(playerChoice, getComputerChoice());
+        console.log(outcome)
+        if (outcome === 'win') humanScore++
+        else if (outcome === 'loss') computerScore++
+        scoreDisplay.textContent = `${humanScore}:${computerScore}`;
+
+        if (humanScore >= rounds || computerScore >= rounds){
+            menuContainer.setAttribute('style', 'display:flex;');
+            gameContainer.setAttribute('style', 'display:none;')
+        }
+    })
+})
